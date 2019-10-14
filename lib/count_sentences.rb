b78436f287d98ba96169498f5b76d78d90f6b14e
David Bermudez <juan.bermudez1102@outlook.com>
2019-10-14 01:28:26 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,41 @@ require 'pry'
 class String
 
   def sentence?
-    
+    array = self.split(" ")
+    if(array[(array.length)-1].include?("."))
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    array = self.split(" ")
+    if(array[(array.length)-1].include?("?"))
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    array = self.split(" ")
+    if(array[(array.length)-1].include?("!"))
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
+    count = 0
+    array = self.split(" ")
+    array.each { |word|
+      if(word.include?(".") || word.include?("!") || word.include?("?"))
+        count += 1
+      end
+    }
 
+    count
   end
 end
