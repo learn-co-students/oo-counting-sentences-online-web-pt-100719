8303b1acbac4b8505f02810f629eafd1fe11ce5e
Adrian Bruno <adrian.j.bruno@gmail.com>
2019-11-07 21:33:32 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -2,19 +2,25 @@ require 'pry'
 
 class String
 
+  # This method takes a in a string and checks it ends with a period.
+  # If the string ends with a period, it comes back 'true'.
+  # If the string does not end with a period, it comes back 'false'.
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = self.split(/[.?!]/).reject do |x|
+      x.empty?
+    end
+    new_array.size
   end
 end
